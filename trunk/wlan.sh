e8cd575b02d2d53ce19614ba4b894777c2aba218
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2004-08-08 21:19:03 +0000

    allow to choose wireless interface
    git-svn-id: https://dl324b-1/svn/shell@13 12a06e64-900f-0410-8321-bc676669c086

--- trunk/wlan.sh
+++ trunk/wlan.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+echo "Wireless interface:"
+echo "a. ath0"
+echo "e. eth1"
+read choice
+if [ $choice = "a" ]; then
+	interface="ath0"
+fi
+if [ $choice = "e" ]; then
+	interface="eth1"
+fi
 echo "Which essid:"
 echo "1. USC"
 echo "2. Math"
@@ -9,42 +19,43 @@ echo "6. belkin54g"
 read a
 if [ $a = "1" ]; then
 	route del default gw src
-	iwconfig ath0 essid "USC"
-	iwconfig ath0 key s:GOUSC
-	dhclient ath0
+	iwconfig $interface essid "USC"
+	iwconfig $interface key s:GOUSC
+	dhclient $interface
 fi
 if [ $a = "2" ]; then
 	route del default gw src
-	iwconfig ath0 essid "Math"
-	dhclient ath0
+	iwconfig $interface essid "Math"
+	dhclient $interface
 fi
 
 if [ $a = "3" ]; then
 	route del default gw src
-	iwconfig ath0 essid "zzhao"
-	iwconfig ath0 key 1234567890
+	iwconfig $interface essid "zzhao"
+	iwconfig $interface key 1234567890
 	ifconfig eth0 10.100.113.107
-	ifconfig ath0 192.168.0.147
-	route add default gw src
+	dhclient $interface
+	#ifconfig $interface 192.168.0.147
+	#route add default gw src
 fi
 
 
 if [ $a = "4" ]; then
 	route del default gw src
-	iwconfig ath0 essid "hto-ww"
+	iwconfig $interface essid "hto-ww"
 	#ifconfig eth0 down
-	dhclient ath0
+	dhclient $interface
 fi
 
 if [ $a = "5" ]; then
 	route del default gw src
-	ifconfig ath0 down
+	ifconfig $interface down
 	dhclient eth0
-	ifconfig ath0 down
+	ifconfig $interface down
 fi
 
 if [ $a = "6" ]; then
 	route del default gw src
-	iwconfig ath0 essid "belkin54g"
-	dhclient ath0
+	iwconfig $interface essid "belkin54g"
+	dhclient $interface
 fi
