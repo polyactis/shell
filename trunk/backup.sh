42d6afd7d0999d07ae137d03b6c0ead2d5d70f93
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2004-10-02 21:02:16 +0000

    improve the comment
    git-svn-id: https://dl324b-1/svn/shell@31 12a06e64-900f-0410-8321-bc676669c086

--- trunk/backup.sh
+++ trunk/backup.sh
@@ -2,7 +2,9 @@
 #argv[1] is the path for the directory.
 #argv[2] is the name of the tar file.
 date
-echo -n 'Starting to backup ...'
+echo -n 'Starting to backup '
+echo -n $1 
+echo -n ' ...'
 tar -cvvf /usr/local/src/zip/$2.tar $1
 gzip /usr/local/src/zip/$2.tar
 scp /usr/local/src/zip/$2.tar.gz yuhuang@hto-g.usc.edu:./backup/
