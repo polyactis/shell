b641a21acb61ff5af367d28649826cb279ef00d5
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2004-10-02 20:55:12 +0000

    copy the file into the ./backup folder on hto-g
    git-svn-id: https://dl324b-1/svn/shell@30 12a06e64-900f-0410-8321-bc676669c086

--- trunk/backup.sh
+++ trunk/backup.sh
@@ -5,7 +5,7 @@ date
 echo -n 'Starting to backup ...'
 tar -cvvf /usr/local/src/zip/$2.tar $1
 gzip /usr/local/src/zip/$2.tar
-scp /usr/local/src/zip/$2.tar.gz yuhuang@hto-g.usc.edu:.
+scp /usr/local/src/zip/$2.tar.gz yuhuang@hto-g.usc.edu:./backup/
 rm /usr/local/src/zip/$2.tar.gz
 echo 'done'
 date
