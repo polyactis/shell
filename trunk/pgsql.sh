09c5d8487e0881006ea340d36bc3c41e156d99c1
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2004-10-02 21:04:47 +0000

    copy to the ./backup foler on hto-g
    git-svn-id: https://dl324b-1/svn/shell@33 12a06e64-900f-0410-8321-bc676669c086

--- trunk/pgsql.sh
+++ trunk/pgsql.sh
@@ -5,7 +5,7 @@ echo -n 'Starting to dump the '
 echo -n $1
 echo -n ' ...'
 pg_dump $1 |gzip >/usr/local/src/zip/$1.gz
-scp /usr/local/src/zip/$1.gz yuhuang@hto-g.usc.edu:./
+scp /usr/local/src/zip/$1.gz yuhuang@hto-g.usc.edu:./backup/
 rm /usr/local/src/zip/$1.gz
 echo -n 'done'
 date
