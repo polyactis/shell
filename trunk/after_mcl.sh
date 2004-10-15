c7db05534beb040a9b426801aebd21d6ed94e114
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2004-10-15 02:05:37 +0000

    comment out the cluster_stat and gene_stat on cluster_table. Also remove mcl_result_stat2 as it's unnecessary. In the third stage pruning, all the stats are copied from mcl_table to mcl_table2.
    git-svn-id: https://dl324b-1/svn/shell@36 12a06e64-900f-0410-8321-bc676669c086

--- trunk/after_mcl.sh
+++ trunk/after_mcl.sh
@@ -22,17 +22,14 @@ echo "Done"
 echo "gene_stat_on_mcl_result"
 ~/script/annot/bin/gene_stat_on_mcl_result.py -k $schema -p 0.001 -m $mcl_table
 echo "Done"
-echo "cluster_stat "
-~/script/annot/bin/cluster_stat.py -k $schema -c -s $mcl_table -t $cluster_table -w -b
-echo "Done"
-echo "gene_stat"
-~/script/annot/bin/gene_stat.py -k $schema -p 0.001 -w -t $cluster_table -m $mcl_table -l
-echo "Done"
+#echo "cluster_stat "
+#~/script/annot/bin/cluster_stat.py -k $schema -c -s $mcl_table -t $cluster_table -w -b
+#echo "Done"
+#echo "gene_stat"
+#~/script/annot/bin/gene_stat.py -k $schema -p 0.001 -w -t $cluster_table -m $mcl_table -l
+#echo "Done"
 echo "Third stage pruning"
 ~/script/annot/bin/cluster_prune.py -k $schema -c -p 2 -s $mcl_table -t $mcl_table2
-echo "Done"
-echo "mcl_result_stat2"
-~/script/annot/bin/mcl_result_stat.py -k $schema -c -t $mcl_table2
 echo "Done"
 echo "gene_stat_on_mcl_result2"
 ~/script/annot/bin/gene_stat_on_mcl_result.py -k $schema -p 0.001 -m $mcl_table2
