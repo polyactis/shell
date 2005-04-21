16200d9d91b51b9b95e682173744e3ab00950c54
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2005-04-21 03:10:23 +0000

    This is shell script simplifying running netmine
    git-svn-id: https://dl324b-1/svn/shell@46 12a06e64-900f-0410-8321-bc676669c086

---
+++ trunk/netmine.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+if test $# -lt 3
+then
+	echo "Usage:"
+	echo "    netmine.sh OUTPUT_SUFFIX ORGANISM PARAMETERS"
+	echo 
+	echo "This is shell script simplifying running netmine"
+	exit
+fi
+
+op=$1
+organism=$2
+parameter=''
+while test -n "$3"
+do
+parameter=$parameter' '$3
+shift
+done
+#parameter is -q -z (-w or -j --match_cut=)
+
+e_graph_fname=F$op\E
+
+if [ $organism = "sc" ]
+then
+	default_parameter="--mp=sc54_5 -n 6661 -p 1342902 -l54 -e6 -g1 -s200"
+	echo "Default parameter is " $default_parameter
+	elif [ $organism = "mm" ]
+	then
+		default_parameter="--mp=mm79_5 -n24305  -p 4088951 -l79 -e6 -g1 -s200"
+		echo "Default parameter is" $default_parameter
+		else
+			echo "Organism not supported."
+			exit
+fi
+
+#the python library path
+source ~/.bash_profile
+date
+
+echo "########I. running netmine##########"
+echo mpirun N ~/script/annot/bin/netmine_wrapper.py $default_parameter $parameter --op=$op
+mpirun N ~/script/annot/bin/netmine_wrapper.py $default_parameter $parameter --op=$op
+
+date
+
+echo "########II. cluster_stat_sc on connected components######"
+echo ~/qjob/cluster_stat.sh sc_54_6661 F$op $organism 1
+~/qjob/cluster_stat.sh sc_54_6661 F$op $organism 1
+date
+
+echo "########III. 2nd-order clusters covering connected components###"
+echo ~/script/annot/bin/EdgeClusterFromCopathOutput.py F$op $e_graph_fname
+cd ~/bin/hhu_clustering/data/output/netmine/
+~/script/annot/bin/EdgeClusterFromCopathOutput.py F$op $e_graph_fname
+
+echo "########IV. cluster_stat_sc on 2nd-order clusters###"
+echo ~/qjob/cluster_stat.sh sc_54_6661 $e_graph_fname $organism 1
+~/qjob/cluster_stat.sh sc_54_6661 $e_graph_fname $organism 1
+date
+
