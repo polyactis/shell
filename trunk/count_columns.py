aceb9ac27a39bef753eeb63c94334589a27b36fd
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2005-05-15 03:23:41 +0000

    count_columns: 	dir is changed to file_list, which = args
    git-svn-id: https://dl324b-1/svn/shell@55 12a06e64-900f-0410-8321-bc676669c086

--- trunk/count_columns.py
+++ trunk/count_columns.py
@@ -1,35 +1,42 @@
 #!/usr/bin/env python
 """
-Usage: count_columns.py [OPTION] DIR
+Usage: count_columns.py [OPTION] FILES
 
 Option:
-	DIR is the directory which contains the files to be renamed.
+	FILES are a list of files whose columns are going to be counted.
 	-d ... --delimiter=...,	delimiter character used to seperate columns, \t(default)
 	-h, --help              show this help
 	
 Examples:
-	count_columns.py gph_result/sc
-	count_columns.py -d ',' gph_result/hs
+	count_columns.py gph_result/sc/*
+	count_columns.py -d ',' /tmp/dataset1
 
 Description:
-	report the number of columns of the first line of all files in DIR
+	report the number of columns of the first line of all files.
 	FUTURE: consider a through check of the whole file.
 """
 
 import sys, os, re, getopt, csv
 
 class count_columns:
-	def __init__(self, dir, delimiter):
-		self.dir = dir
-		self.files = os.listdir(dir)
+	def __init__(self, file_list, delimiter):
+		"""
+		05-14-05
+			dir is changed to file_list
+		"""
+		self.files = file_list
 		self.files.sort()
 		self.delimiter = delimiter
 
 	def run(self):
+		"""
+		05-14-05
+			f is directly the src_pathname
+		"""
 		sys.stderr.write("\tTotally, %d files to be processed.\n"%len(self.files))
 		for f in self.files:
 			sys.stderr.write("%d/%d:\t%s"%(self.files.index(f)+1,len(self.files),f))
-			src_pathname = os.path.join(self.dir, f)
+			src_pathname = f
 			reader = csv.reader(file(src_pathname), delimiter=self.delimiter)
 			try:
 				row = reader.next()
@@ -59,8 +66,8 @@ if __name__ == '__main__':
 		elif opt in ("-d", "--delimiter"):
 			delimiter = arg
 
-	if len(args)==1:
-		instance = count_columns(args[0], delimiter)
+	if len(args)>=1:
+		instance = count_columns(args, delimiter)
 		instance.run()
 	else:
 		print __doc__
