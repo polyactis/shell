3349fe751344a10c393abf87d7546bffee220b69
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2005-05-20 08:41:01 +0000

    #05-20-05 add graph_reorganize.py
    git-svn-id: https://dl324b-1/svn/shell@65 12a06e64-900f-0410-8321-bc676669c086

--- trunk/SchemaPreSetup.sh
+++ trunk/SchemaPreSetup.sh
@@ -14,6 +14,8 @@ schema=$2
 unknown_file=/tmp/$organism.unknown
 go_file=/tmp/$schema.go
 datasets_dir=~/datasets/$schema
+gph_dir=~/gph_result/$schema
+gph_dir_gspan=~/gph_result/$schema\_gspan
 
 #the python library path
 source ~/.bash_profile
@@ -33,5 +35,8 @@ echo ~/script/annot/bin/go_bioprocess.py -k $schema -p min -u $unknown_file -c $
 
 echo ~/script/annot/bin/gene_go_functions.py -k $schema -c
 ~/script/annot/bin/gene_go_functions.py -k $schema -c
+#05-20-05 add graph_reorganize.py
+echo ~/script/annot/bin/graph_reorganize.py -k $schema -t1 -g $organism $gph_dir $gph_dir_gspan 
+~/script/annot/bin/graph_reorganize.py -k $schema -t1 -g $organism $gph_dir $gph_dir_gspan 
 
 date
