945dace62e5f4b577155c6f4395a075ad25c960f
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2005-05-20 18:15:22 +0000

    #05-19-05 cluster_stat goes to a file #05-20-05 no more schema guessing
    git-svn-id: https://dl324b-1/svn/shell@68 12a06e64-900f-0410-8321-bc676669c086

--- trunk/cluster_stat.sh
+++ trunk/cluster_stat.sh
@@ -20,22 +20,27 @@ codenOrcopath=$4
 #2 is coden
 splat_result_table=splat_$2
 mcl_result_table=mcl_$2
-cluster_stat_table=cluster_$2
+#05-19-05 cluster_stat goes to a file
+cluster_stat_table=/scratch/00/yuhuang/cluster_stat/cluster_$2
 p_gene_table=p_gene_$2_e5
 gene_p_table=gene_p_$2_e5_p01
 
-case "$schema" in
-	sc_new_38)	gene_id2no=sc_54_6661_gene_id2no
-		echo $gene_id2no;;
-	sc_54_6661)	gene_id2no=sc_54_6661_gene_id2no
-		echo $gene_id2no;;
-	mm_79)	gene_id2no="mm_79_gene_id2no"
-		echo $gene_id2no;;
-	mm_73)	gene_id2no="mm_73_gene_id2no"
-		echo $gene_id2no;;
-	*)	echo "Schema" $schema "not supported"
-		exit 2;;
-esac
+gene_id2no=$schema\_gene_id2no
+echo $gene_id2no
+#05-20-05 no more schema guessing
+
+#case "$schema" in
+#	sc_new_38)	gene_id2no=sc_54_6661_gene_id2no
+#		echo $gene_id2no;;
+#	sc_54_6661)	gene_id2no=sc_54_6661_gene_id2no
+#		echo $gene_id2no;;
+#	mm_79)	gene_id2no="mm_79_gene_id2no"
+#		echo $gene_id2no;;
+#	mm_73)	gene_id2no="mm_73_gene_id2no"
+#		echo $gene_id2no;;
+#	*)	echo "Schema" $schema "not supported"
+#		exit 2;;
+#esac
 
 
 #the python library path
@@ -52,14 +57,15 @@ then
 	~/script/annot/bin/codense/codense2db.py -k $1 -p ~/bin/hhu_clustering/$gene_id2no -c -y$4 -t $splat_result_table -m $mcl_result_table $2
 fi
 
-echo ~/script/annot/bin/cluster_stat.py -k $1 -s $mcl_result_table  -t $cluster_stat_table -b -w -c
-~/script/annot/bin/cluster_stat.py -k $1 -s $mcl_result_table  -t $cluster_stat_table -b -w -c
-echo ~/script/annot/bin/gene_stat.py -k $1 -t $cluster_stat_table -m $mcl_result_table -g $p_gene_table -e 5 -l -w -c
-~/script/annot/bin/gene_stat.py -k $1 -t $cluster_stat_table -m $mcl_result_table -g $p_gene_table -e 5 -l -w -c
+#05-19-05 cluster_stat goes to a file
+echo ~/script/annot/bin/cluster_stat.py -k $1 -s $mcl_result_table  -p $cluster_stat_table -b -w
+~/script/annot/bin/cluster_stat.py -k $1 -s $mcl_result_table  -p $cluster_stat_table -b -w
+#05-19-05 cluster_stat goes to a file
+echo ~/script/annot/bin/gene_stat.py -k $1 -f $cluster_stat_table -m $mcl_result_table -g $p_gene_table -e 5 -l -w -c
+~/script/annot/bin/gene_stat.py -k $1 -f $cluster_stat_table -m $mcl_result_table -g $p_gene_table -e 5 -l -w -c
 echo ~/script/annot/bin/p_gene_analysis.py -k $1 -p 0.01 -c -j 2  -g $p_gene_table -n $gene_p_table ~/p_gene_analysis/$gene_p_table.out
 ~/script/annot/bin/p_gene_analysis.py -k $1 -p 0.01 -c -j 2  -g $p_gene_table -n $gene_p_table ~/p_gene_analysis/$gene_p_table.out
 echo ~/script/annot/bin/gene_p_map_redundancy.py -k $1 -t $p_gene_table -n $gene_p_table -c
 ~/script/annot/bin/gene_p_map_redundancy.py -k $1 -t $p_gene_table -n $gene_p_table -c
 echo ~/script/annot/bin/connectivity_original.py -k $1 -c -t $mcl_result_table
-~/script/annot/bin/connectivity_original.py -k $1 -c -t $mcl_result_table
-date
+~/script/annot/bin/connectivity_original.py 
