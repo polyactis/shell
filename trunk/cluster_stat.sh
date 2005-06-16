eba3d967512eeaa4d4a858086b4f54ac92b09020
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2005-06-16 03:51:37 +0000

    add trailing parameters to the file, passed to cluster_stat.py rearrange the parameter handling part
    git-svn-id: https://dl324b-1/svn/shell@78 12a06e64-900f-0410-8321-bc676669c086

--- trunk/cluster_stat.sh
+++ trunk/cluster_stat.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-if test $# -ne 3
+if test $# -lt 3
 then
 	echo "Usage:"
-	echo "    cluster_stat.sh SCHEMA INPUT_FILE RUNCODE"
+	echo "    cluster_stat.sh SCHEMA INPUT_FILE RUNCODE PARAMETERS"
 	echo
 	echo "This is a script linking all stat programs"
+	echo "PARAMETERS are passed to cluster_stat.py"
+	echo "Except -k -s -p -b -w -u, others are ok, like -n and -e."
 	echo
 	echo "RUNCODE controls which part to turn on"
 	echo " 1.codense2db 2.cluster_stat.py"
@@ -20,8 +22,6 @@ fi
 schema=$1
 input_file=$2
 runcode=$3
-
-echo " RUNCODE is $runcode "
 
 type_1=`echo $runcode|awk '{print substr($0,1,1)}'`	#{} is a must.
 type_2=`echo $runcode|awk '{print substr($0,2,1)}'`
@@ -40,48 +40,59 @@ gene_id2no=$schema\_gene_id2no
 echo $gene_id2no
 
 
+parameter=''
+while test -n "$4"
+do
+parameter=$parameter' '$4
+shift
+done
+
+echo " RUNCODE is $runcode "
+echo "parameter to cluster_stat.py is $parameter"
+
+
 #the python library path
 source ~/.bash_profile
 date
 cd ~/bin/hhu_clustering/data/output/netmine/
 
 case "$type_1" in
-	1)	echo ~/script/annot/bin/codense/codense2db.py -k $1 -p ~/bin/hhu_clustering/$gene_id2no -c -y1 -t $splat_result_table -m $mcl_result_table $2
-		~/script/annot/bin/codense/codense2db.py -k $1 -p ~/bin/hhu_clustering/$gene_id2no -c -y1 -t $splat_result_table -m $mcl_result_table $2;;
-	2)	echo ~/script/annot/bin/codense/codense2db.py -k $1 -p ~/bin/hhu_clustering/$gene_id2no -c -y2 -t $splat_result_table -m $mcl_result_table $2
-		~/script/annot/bin/codense/codense2db.py -k $1 -p ~/bin/hhu_clustering/$gene_id2no -c -y2 -t $splat_result_table -m $mcl_result_table $2;;
-	3)	echo ~/script/annot/bin/codense/codense2db.py -k $1 -c -t $splat_result_table -m $mcl_result_table $2
-		~/script/annot/bin/codense/codense2db.py -k $1 -c -t $splat_result_table -m $mcl_result_table $2;;
+	1)	echo ~/script/annot/bin/codense/codense2db.py -k $schema -p ~/bin/hhu_clustering/$gene_id2no -c -y1 -t $splat_result_table -m $mcl_result_table $input_file
+		~/script/annot/bin/codense/codense2db.py -k $schema -p ~/bin/hhu_clustering/$gene_id2no -c -y1 -t $splat_result_table -m $mcl_result_table $input_file;;
+	2)	echo ~/script/annot/bin/codense/codense2db.py -k $schema -p ~/bin/hhu_clustering/$gene_id2no -c -y2 -t $splat_result_table -m $mcl_result_table $input_file
+		~/script/annot/bin/codense/codense2db.py -k $schema -p ~/bin/hhu_clustering/$gene_id2no -c -y2 -t $splat_result_table -m $mcl_result_table $input_file;;
+	3)	echo ~/script/annot/bin/codense/codense2db.py -k $schema -c -t $splat_result_table -m $mcl_result_table $input_file
+		~/script/annot/bin/codense/codense2db.py -k $schema -c -t $splat_result_table -m $mcl_result_table $input_file;;
 	*)	echo "codense2db skipped";;
 esac
 
 #05-19-05 cluster_stat goes to a file
 
 if [ $type_2 = "1" ]; then
-	echo ssh node24 ~/script/annot/bin/cluster_stat.py -k $1 -s $mcl_result_table  -p $cluster_stat_table -b -w -u 0
-	ssh node24 ~/script/annot/bin/cluster_stat.py -k $1 -s $mcl_result_table  -p $cluster_stat_table -b -w -u 0
+	echo ssh node24 ~/script/annot/bin/cluster_stat.py -k $schema -s $mcl_result_table  -p $cluster_stat_table -b -w -u 0 $parameter
+	ssh node24 ~/script/annot/bin/cluster_stat.py -k $schema -s $mcl_result_table  -p $cluster_stat_table -b -w -u 0 $parameter
 fi
 
 if [ $type_3 = "1" ]; then
 	#05-19-05 cluster_stat goes to a file
-	echo ~/script/annot/bin/gene_stat.py -k $1 -f $cluster_stat_table -m $mcl_result_table -g $p_gene_table -e 5 -l -w -c
-	~/script/annot/bin/gene_stat.py -k $1 -f $cluster_stat_table -m $mcl_result_table -g $p_gene_table -e 5 -l -w -c
+	echo ~/script/annot/bin/gene_stat.py -k $schema -f $cluster_stat_table -m $mcl_result_table -g $p_gene_table -e 5 -l -w -c
+	~/script/annot/bin/gene_stat.py -k $schema -f $cluster_stat_table -m $mcl_result_table -g $p_gene_table -e 5 -l -w -c
 fi
 
 if [ $type_4 = "1" ]; then
-	echo ~/script/annot/bin/p_gene_analysis.py -k $1 -p 0.01 -c -j 2  -g $p_gene_table -n $gene_p_table ~/p_gene_analysis/$gene_p_table.out
-	~/script/annot/bin/p_gene_analysis.py -k $1 -p 0.01 -c -j 2  -g $p_gene_table -n $gene_p_table ~/p_gene_analysis/$gene_p_table.out
+	echo ~/script/annot/bin/p_gene_analysis.py -k $schema -p 0.01 -c -j 2  -g $p_gene_table -n $gene_p_table ~/p_gene_analysis/$gene_p_table.out
+	~/script/annot/bin/p_gene_analysis.py -k $schema -p 0.01 -c -j 2  -g $p_gene_table -n $gene_p_table ~/p_gene_analysis/$gene_p_table.out
 fi
 
 
 if [ $type_5 = "1" ]; then
-	echo ~/script/annot/bin/gene_p_map_redundancy.py -k $1 -t $p_gene_table -n $gene_p_table -c
-	~/script/annot/bin/gene_p_map_redundancy.py -k $1 -t $p_gene_table -n $gene_p_table -c
+	echo ~/script/annot/bin/gene_p_map_redundancy.py -k $schema -t $p_gene_table -n $gene_p_table -c
+	~/script/annot/bin/gene_p_map_redundancy.py -k $schema -t $p_gene_table -n $gene_p_table -c
 fi
 
 
 if [ $type_6 = "1" ]; then
-	echo ~/script/annot/bin/connectivity_original.py -k $1 -c -t $mcl_result_table
-	~/script/annot/bin/connectivity_original.py -k $1 -c -t $mcl_result_table
+	echo ~/script/annot/bin/connectivity_original.py -k $schema -c -t $mcl_result_table
+	~/script/annot/bin/connectivity_original.py -k $schema -c -t $mcl_result_table
 fi
 date
