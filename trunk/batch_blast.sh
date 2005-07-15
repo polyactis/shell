ebc23212f868ac74dd6b60638abb011f12c7fc22
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2005-07-15 00:14:25 +0000

    add more comment and the file-splitting step
    git-svn-id: https://dl324b-1/svn/shell@84 12a06e64-900f-0410-8321-bc676669c086

--- trunk/batch_blast.sh
+++ trunk/batch_blast.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
+#split the sequence file of arabidopsis (fasta format) into pieces and blast them against NCBI
+#the reason to split is that blastcl3 can't deal with file with many sequences, it'll skip some of them.
+
+echo "Make directories, at_seq_to_blast and blast_result"
+mkdir ~/at_seq_to_blast
+mkdir ~/blast_result
 cd ~/at_seq_to_blast
 
+echo "Splitting sequence files into pieces"
+split -a 3 -l 44 ~/unigene_linking/at_probeset_seq
+
+echo "Start to blast"
 for i in $(ls )
 do
 	if test -r $i
