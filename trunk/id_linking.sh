518d757df5f851746ba8b6b829212537400dd89f
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2005-08-30 22:05:19 +0000

    from one input table probe_information_center output table prefix is p2u.
    git-svn-id: https://dl324b-1/svn/shell@103 12a06e64-900f-0410-8321-bc676669c086

--- trunk/id_linking.sh
+++ trunk/id_linking.sh
@@ -4,10 +4,11 @@ then
 	echo "Usage:"
 	echo "    id_linking.sh organism organismU"
 	echo 
-	echo " organism is small case two-letter"
-	echo " organismU is like At, Cel, Dm, Hs"
+	echo " organism is like at, ce, dm, hs, mm, rn"
+	echo " organismU is like At, Cel, Dm, Hs, Mm, Rn"
 	echo " call MdbId2UnigeneId.py to link id to Unigene id."
-	echo " first suid_info; then probe_information_center"
+	echo " Input table is probe_information_center."
+	echo " Output table prefix is p2u."
 	echo " Gene and Unigene data files are in /scratch/00/yuhuang/tmp/"
 	echo
 	exit
@@ -20,12 +21,7 @@ organismU=$2
 source ~/.bash_profile
 date
 
-echo ~/script/microarray/bin/MdbId2UnigeneId.py -i suid_info -g $organism -p $organism\_unigene -o /scratch/00/yuhuang/tmp/gene_info -a /scratch/00/yuhuang/tmp/gene2accession -w /scratch/00/yuhuang/tmp/$organismU.data -x /scratch/00/yuhuang/tmp/$organismU.retired.lst -n -c
-~/script/microarray/bin/MdbId2UnigeneId.py -i suid_info -g $organism -p $organism\_unigene -o /scratch/00/yuhuang/tmp/gene_info -a /scratch/00/yuhuang/tmp/gene2accession -w /scratch/00/yuhuang/tmp/$organismU.data -x /scratch/00/yuhuang/tmp/$organismU.retired.lst -n -c
-
-date
-
-echo ~/script/microarray/bin/MdbId2UnigeneId.py -i probe_information_center -g $organism -p $organism\_unigene -o /scratch/00/yuhuang/tmp/gene_info -a /scratch/00/yuhuang/tmp/gene2accession -w /scratch/00/yuhuang/tmp/$organismU.data -x /scratch/00/yuhuang/tmp/$organismU.retired.lst -c
-~/script/microarray/bin/MdbId2UnigeneId.py -i probe_information_center -g $organism -p $organism\_unigene -o /scratch/00/yuhuang/tmp/gene_info -a /scratch/00/yuhuang/tmp/gene2accession -w /scratch/00/yuhuang/tmp/$organismU.data -x /scratch/00/yuhuang/tmp/$organismU.retired.lst -c
+echo ~/script/microarray/bin/MdbId2UnigeneId.py -i probe_information_center -g $organism -p p2u -o /scratch/00/yuhuang/tmp/gene_info -a /scratch/00/yuhuang/tmp/gene2accession -w /scratch/00/yuhuang/tmp/$organismU.data -x /scratch/00/yuhuang/tmp/$organismU.retired.lst -c
+~/script/microarray/bin/MdbId2UnigeneId.py -i probe_information_center -g $organism -p p2u -o /scratch/00/yuhuang/tmp/gene_info -a /scratch/00/yuhuang/tmp/gene2accession -w /scratch/00/yuhuang/tmp/$organismU.data -x /scratch/00/yuhuang/tmp/$organismU.retired.lst -c
 
 date
