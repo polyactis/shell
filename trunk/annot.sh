266937f7c6798d25c93a0a20b92beab4f1c7af2c
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2005-09-20 01:14:24 +0000

    fix some echo ###... add fim_wrapper.sh
    git-svn-id: https://dl324b-1/svn/shell@110 12a06e64-900f-0410-8321-bc676669c086

--- trunk/annot.sh
+++ trunk/annot.sh
@@ -6,7 +6,13 @@ then
 	echo ""
 	echo "RUNCODE's digits control:"
 	echo "  1 SchemaDatasetSetup.py, 2 SchemaPreSetup.sh"
-	echo "  3 graph_merge.sh, 4 netmine.sh"
+	echo "  3 graph_merge.sh, 4 netmine.sh or fim_wrapper.sh"
+	echo " For graph_merge.sh:"
+	echo "	1 graph_merge_lam.py(non-qsub)+complete_cor_vector(non-qsub)"
+	echo "	2 graph_merge.py+complete_cor_vector.py(qsub)"
+	echo "	3 graph_merge_lam.py(qsub)+complete_cor_vector(qsub)"
+	echo " For the 4th digit:"
+	echo "	1 netmine.sh, 2 fim_wrapper.sh(qsub)"
 	echo
 	exit
 fi
@@ -25,25 +31,31 @@ type_4=`echo $runcode|awk '{print substr($0,4,1)}'`
 source ~/.bash_profile
 
 case "$type_1" in
-	1)	echo ########## ~/script/shell/SchemaDatasetSetup.py -k $schema  -g $organism -f ~/datasets/mapping/$schema ##########
+	1)	echo "########## ~/script/shell/SchemaDatasetSetup.py -k $schema  -g $organism -f ~/datasets/mapping/$schema ##########"
 		~/script/shell/SchemaDatasetSetup.py -k $schema  -g $organism -f ~/datasets/mapping/$schema;;
 	*)	echo "SchemaDatasetSetup.py skipped";;
 esac
 
 case "$type_2" in
-	1)	echo ########## ~/script/shell/SchemaPreSetup.sh $organism $schema 1111111 ######
+	1)	echo "########## ~/script/shell/SchemaPreSetup.sh $organism $schema 1111111 ######"
 		~/script/shell/SchemaPreSetup.sh $organism $schema 1111111;;
 	*)	echo "SchemaPreSetup.sh skipped";;
 esac
 
 case "$type_3" in
-	1)	echo ########## ~/script/shell/graph_merge.sh $min_support $schema 1111 #######
-		~/script/shell/graph_merge.sh $min_support $schema 2111;;
+	1)	echo "########## ssh ~/script/shell/graph_merge.sh $min_support $schema 1111 #######"
+		ssh ~/script/shell/graph_merge.sh $min_support $schema 2111;;
+	2)	echo "########## ~/script/shell/graph_merge.sh $min_support $schema 1211 #######"
+		~/script/shell/graph_merge.sh $min_support $schema 1211;;
+	3)	echo "########## ~/script/shell/graph_merge.sh $min_support $schema 3211 #######"
+		~/script/shell/graph_merge.sh $min_support $schema 3211;;
 	*)	echo "graph_merge.sh skipped";;
 esac
 
 case "$type_4" in
-	1)	echo ########## ~/script/shell/netmine.sh $schema\g1e3d40q50s200c50z0001c8 $schema 2 220 -e3 -q0.5 -z0001 -t corTable0.8 ########
+	1)	echo "########## ~/script/shell/netmine.sh $schema\g1e3d40q50s200c50z0001c8 $schema 2 220 -e3 -q0.5 -z0001 -t corTable0.8 ########"
 		~/script/shell/netmine.sh $schema\g1e3d40q50s200c50z0001c8 $schema 2 220 0.6 -e3 -q0.5 -z0001 -t corTable0.8;;
-	*)	echo "netmine.sh skipped";;
+	2)	echo "########## ~/script/shell/fim_wrapper.sh $schema $min_support 200 1111 0.6######"
+		~/script/shell/fim_wrapper.sh $schema $min_support 200 1111 0.6;;
+	*)	echo "netmine.sh or fim_wrapper.sh skipped";;
 esac
