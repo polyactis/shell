2d0bb5c49406e05d98d356fa1c73af7ad51d9545
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2005-10-04 21:20:34 +0000

    split p_gene_lm.sh and p_gene_analysis.py add check_exit_status
    git-svn-id: https://dl324b-1/svn/shell@123 12a06e64-900f-0410-8321-bc676669c086

--- trunk/cluster_stat2.sh
+++ trunk/cluster_stat2.sh
@@ -8,12 +8,12 @@ then
 	echo "This is a script linking all stat programs"
 	echo
 	echo "RUNCODE controls which part to turn on"
-	echo " 1.p_gene_lm/p_gene_analysis 2.gene_p_map_redundancy"
-	echo " 3.filter.sh"
+	echo " 1.p_gene_lm 2.p_gene_analysis"
+	echo " 3.gene_p_map_redundancy 4.filter.sh"
 	echo 
-	echo " 1st digit: 1(p_gene_lm + p_gene_analysis)"
-	echo "   2(p_gene_analysis)"
-	echo " 3rd digit: 1(qsub), 2(direct run)"
+	echo " 2nd digit: 1(from lm_table)"
+	echo "   2(p_value 0.01)"
+	echo " 4th digit: 1(qsub), 2(direct run)"
 	exit
 fi
 
@@ -25,6 +25,7 @@ acc_cutoff=$4
 type_1=`echo $runcode|awk '{print substr($0,1,1)}'`	#{} is a must.
 type_2=`echo $runcode|awk '{print substr($0,2,1)}'`
 type_3=`echo $runcode|awk '{print substr($0,3,1)}'`
+type_4=`echo $runcode|awk '{print substr($0,4,1)}'`
 
 splat_result_table=splat_$input_file
 mcl_result_table=mcl_$input_file
@@ -37,6 +38,14 @@ gene_p_table=gene_p_$input_file\_e5_a$acc_int
 
 echo " RUNCODE is $runcode "
 
+check_exit_status() {
+	date
+	return_code=$?
+	if [ $return_code != "0" ]; then
+		echo "Return code non-zero:"$return_code
+		exit
+	fi
+}
 
 #the python library path
 source ~/.bash_profile
@@ -44,26 +53,33 @@ source ~/.bash_profile
 date
 
 case "$type_1" in
-	1)	echo ssh node27 ~/script/shell/p_gene_lm.sh $schema $input_file $acc_cutoff
-		ssh node27 ~/script/shell/p_gene_lm.sh $schema $input_file $acc_cutoff
-		#p_gene_lm calls rpy.r, which is banned by qsub. run it elsewhere
-		echo ~/script/annot/bin/p_gene_analysis.py -k $schema -t $splat_result_table -p 0 -l $lm_table -c -j 2  -g $p_gene_table -n $gene_p_table ~/p_gene_analysis/$gene_p_table.out
+	1)	echo ssh $HOSTNAME ~/script/shell/p_gene_lm.sh $schema $input_file $acc_cutoff
+		ssh $HOSTNAME ~/script/shell/p_gene_lm.sh $schema $input_file $acc_cutoff;;
+	*)	echo "No p_gene_lm.sh";;
+esac
+
+check_exit_status
+
+case "$type_2" in
+	1)	echo ~/script/annot/bin/p_gene_analysis.py -k $schema -t $splat_result_table -p 0 -l $lm_table -c -j 2  -g $p_gene_table -n $gene_p_table ~/p_gene_analysis/$gene_p_table.out
 		~/script/annot/bin/p_gene_analysis.py -k $schema -t $splat_result_table -p 0 -l $lm_table -c -j 2  -g $p_gene_table -n $gene_p_table ~/p_gene_analysis/$gene_p_table.out;;
 	2)	echo ~/script/annot/bin/p_gene_analysis.py -k $schema -t $splat_result_table -p 0.01 -c -j 2  -g $p_gene_table -n $gene_p_table ~/p_gene_analysis/$gene_p_table.out
 		~/script/annot/bin/p_gene_analysis.py -k $schema -t $splat_result_table -p 0.01 -c -j 2  -g $p_gene_table -n $gene_p_table ~/p_gene_analysis/$gene_p_table.out;;
-	*)	echo "No p_gene_analysis and/or p_gene_lm";;
+	*)	echo "No p_gene_analysis.py";;
+
 esac
 
-date
+check_exit_status
 
-if [ $type_2 = "1" ]; then
+
+if [ $type_3 = "1" ]; then
 	echo ~/script/annot/bin/gene_p_map_redundancy.py -k $schema -t $p_gene_table -n $gene_p_table -c
 	~/script/annot/bin/gene_p_map_redundancy.py -k $schema -t $p_gene_table -n $gene_p_table -c
 fi
 
-date
+check_exit_status
 
-case "$type_1" in
+case "$type_4" in
 	1)	echo ~/script/shell/filter.sh $schema $input_file $acc_cutoff 11
 		~/script/shell/filter.sh $schema $input_file $acc_cutoff 11;;
 	2)	echo ~/script/shell/filter.sh $schema $input_file $acc_cutoff 22
@@ -74,4 +90,4 @@ esac
 #echo ~/script/annot/bin/connectivity_original.py -k $schema -c -t $mcl_result_table
 #~/script/annot/bin/connectivity_original.py -k $schema -c -t $mcl_result_table
 
-date
+check_exit_status
