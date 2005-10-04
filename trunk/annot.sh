4180ed8cb347f1e6aa62045c2c704c3559255396
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2005-10-04 20:27:36 +0000

    add check_exit_status add one more way to run graph_merge.sh add min gene frequency extend the runcode for fim_wrapper.sh
    git-svn-id: https://dl324b-1/svn/shell@119 12a06e64-900f-0410-8321-bc676669c086

--- trunk/annot.sh
+++ trunk/annot.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-if test $# -ne 4
+if test $# -ne 5
 then
 	echo "Usage:"
-	echo "   annot.sh ORGANISM SCHEMA MIN_SUPPORT RUNCODE"
+	echo "   annot.sh ORGANISM SCHEMA MIN_SUPPORT MIN_GENE_FREQ RUNCODE"
 	echo ""
 	echo "RUNCODE's digits control:"
 	echo "  1 SchemaDatasetSetup.py, 2 SchemaPreSetup.sh"
 	echo "  3 graph_merge.sh, 4 netmine.sh or fim_wrapper.sh"
 	echo " For graph_merge.sh:"
 	echo "	1 graph_merge_lam.py(non-qsub)+complete_cor_vector(non-qsub)"
-	echo "	2 graph_merge.py+complete_cor_vector.py(qsub)"
-	echo "	3 graph_merge_lam.py(qsub)+complete_cor_vector(qsub)"
+	echo "	2 graph_merge_lam.py(qsub)+complete_cor_vector(qsub)"
+	echo "	3 graph_merge.py+complete_cor_vector.py(qsub)"
+	echo "	4 graph_merge.py+complete_cor_vector.py(non-qsub)"
 	echo " For the 4th digit:"
 	echo "	1 netmine.sh, 2 fim_wrapper.sh(qsub)"
 	echo
@@ -20,15 +21,26 @@ fi
 organism=$1
 schema=$2
 min_support=$3
-runcode=$4
+min_gene_freq=$4
+runcode=$5
 #08-16-05 use runcode to control which step is necessary
 type_1=`echo $runcode|awk '{print substr($0,1,1)}'`	#{} is a must.
 type_2=`echo $runcode|awk '{print substr($0,2,1)}'`
 type_3=`echo $runcode|awk '{print substr($0,3,1)}'`
 type_4=`echo $runcode|awk '{print substr($0,4,1)}'`
 
+check_exit_status() {
+	return_code=$?
+	if [ $return_code != "0" ]; then
+		echo "Return code non-zero:"$return_code
+		exit
+	fi
+}
+
 #the python library path
 source ~/.bash_profile
+
+date
 
 case "$type_1" in
 	1)	echo "########## ~/script/shell/SchemaDatasetSetup.py -k $schema  -g $organism -f ~/datasets/mapping/$schema ##########"
@@ -36,26 +48,39 @@ case "$type_1" in
 	*)	echo "SchemaDatasetSetup.py skipped";;
 esac
 
+check_exit_status
+date
+
 case "$type_2" in
-	1)	echo "########## ~/script/shell/SchemaPreSetup.sh $organism $schema 1111111 ######"
-		~/script/shell/SchemaPreSetup.sh $organism $schema 1111111;;
+	1)	echo "########## ~/script/shell/SchemaPreSetup.sh $organism $schema $min_gene_freq 1111111 ######"
+		~/script/shell/SchemaPreSetup.sh $organism $schema $min_gene_freq 1111111;;
 	*)	echo "SchemaPreSetup.sh skipped";;
 esac
+
+check_exit_status
+
+date
 
 case "$type_3" in
 	1)	echo "########## ssh ~/script/shell/graph_merge.sh $min_support $schema 1111 #######"
 		ssh ~/script/shell/graph_merge.sh $min_support $schema 2111;;
-	2)	echo "########## ~/script/shell/graph_merge.sh $min_support $schema 1211 #######"
-		~/script/shell/graph_merge.sh $min_support $schema 1211;;
-	3)	echo "########## ~/script/shell/graph_merge.sh $min_support $schema 3211 #######"
+	2)	echo "########## ~/script/shell/graph_merge.sh $min_support $schema 3211 #######"
 		~/script/shell/graph_merge.sh $min_support $schema 3211;;
+	3)	echo "########## ~/script/shell/graph_merge.sh $min_support $schema 1211 #######"
+		~/script/shell/graph_merge.sh $min_support $schema 1211;;
+	4)	echo "########## ~/script/shell/graph_merge.sh $min_support $schema 1111 #######"
+		~/script/shell/graph_merge.sh $min_support $schema 1111;;
 	*)	echo "graph_merge.sh skipped";;
 esac
+
+check_exit_status
+
+date
 
 case "$type_4" in
 	1)	echo "########## ~/script/shell/netmine.sh $schema\g1e3d40q50s200c50z0001c8 $schema 2 220 -e3 -q0.5 -z0001 -t corTable0.8 ########"
 		~/script/shell/netmine.sh $schema\g1e3d40q50s200c50z0001c8 $schema 2 220 0.6 -e3 -q0.5 -z0001 -t corTable0.8;;
-	2)	echo "########## ~/script/shell/fim_wrapper.sh $schema $min_support 200 1111 0.6######"
-		~/script/shell/fim_wrapper.sh $schema $min_support 200 1111 0.6;;
+	2)	echo "########## ~/script/shell/fim_wrapper.sh $schema $min_support 200 111111 0.6######"
+		~/script/shell/fim_wrapper.sh $schema $min_support 200 111111 0.6;;
 	*)	echo "netmine.sh or fim_wrapper.sh skipped";;
 esac
