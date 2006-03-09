52b535be5db1f4048f4851204de5f1fa51888a35
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2006-03-09 01:02:54 +0000

    change the default host to be hpc-cmb.usc.edu
    git-svn-id: https://dl324b-1/svn/shell@175 12a06e64-900f-0410-8321-bc676669c086

--- trunk/backup.sh
+++ trunk/backup.sh
@@ -5,9 +5,15 @@ date
 echo -n 'Starting to backup '
 echo -n $1 
 echo -n ' ...'
+if test -n "$3"
+then
+	host_name=$3
+else
+	host_name=hpc-cmb.usc.edu
+fi
 tar -cvvf /usr/local/src/zip/$2.tar $1
 gzip /usr/local/src/zip/$2.tar
-scp /usr/local/src/zip/$2.tar.gz yuhuang@app2.cmb.usc.edu:./backup/
+scp /usr/local/src/zip/$2.tar.gz yuhuang@$host_name:./backup/
 rm /usr/local/src/zip/$2.tar.gz
 echo 'done'
 date
--- trunk/pgsql.sh
+++ trunk/pgsql.sh
@@ -3,8 +3,14 @@ date
 echo -n 'Starting to dump the '
 echo -n $1
 echo -n ' ...'
+if test -n "$2"
+then
+	host_name=$2
+else
+	host_name=hpc-cmb.usc.edu
+fi
 pg_dump $1 |gzip >/usr/local/src/zip/$1.gz
-scp /usr/local/src/zip/$1.gz yuhuang@app1.cmb.usc.edu:./backup/
+scp /usr/local/src/zip/$1.gz yuhuang@$host_name:./backup/
 rm /usr/local/src/zip/$1.gz
 echo -n 'done'
 date
