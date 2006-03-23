23bd5f91604005cc34f92fc019482e43eda9dbdb
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2006-03-23 19:51:13 +0000

    reformat the interface
    git-svn-id: https://dl324b-1/svn/shell@176 12a06e64-900f-0410-8321-bc676669c086

--- trunk/mpi_rpart_valid.sh
+++ trunk/mpi_rpart_valid.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if test $# -lt 6
+if test $# -lt 4
 then
-	echo "Usage: mpi_rpart_valid.sh SCHEMA INFNAME OUTPUTPREFIX cp_ls loss_ls prior_ls no_of_validations runcode"
+	echo "Usage: mpi_rpart_valid.sh SCHEMA INFNAME OUTPUTPREFIX RUNCODE PARAMETERS"
 	echo
 	echo " runcode"
 	echo " 1: parallel hpc-cmb, 2: qsub, 3: 6 nodes from ~/hostfile"
@@ -13,32 +13,34 @@ fi
 schema=$1
 infname=$2
 output_prefix=$3
-cp_ls=$4
-loss_ls=$5
-prior_ls=$6
-no_of_validations=$7
-runcode=$8
+runcode=$4
+parameter=''
+while test -n "$5"
+do
+parameter=$parameter' '$5
+shift
+done
 
-output_file=~/MpiRpartValidation_out/$output_prefix\p$cp_ls\l$loss_ls\o$prior_ls\x$no_of_validations
+output_file=~/MpiRpartValidation_out/$output_prefix`~/script/annot/bin/arguments2string.py $parameter`
 type_1=`echo $runcode|awk '{print substr($0,1,1)}'`     #{} is a must.
 
 date
 case "$type_1" in
-	1)	echo mpiexec ~/script/annot/bin/MpiRpartValidation.py -k $schema -i $infname -j $output_file  -p $cp_ls -l $loss_ls -o $prior_ls -x $no_of_validations -s 0.9
+	1)	echo mpiexec ~/script/annot/bin/MpiRpartValidation.py -k $schema -i $infname -j $output_file  $parameter
 		#parallel for hpc-cmb
-	mpiexec ~/script/annot/bin/MpiRpartValidation.py -k $schema -i $infname -j $output_file  -p $cp_ls -l $loss_ls -o $prior_ls -x $no_of_validations -s 0.9;;
+	mpiexec ~/script/annot/bin/MpiRpartValidation.py -k $schema -i $infname -j $output_file  $parameter;;
 	2)	n_hosts=$NSLOTS
 	old_machinefile=$TMPDIR/machines
 	machinefile=~/machines.$JOB_ID
 	cp $old_machinefile $machinefile
-	echo ssh $HOSTNAME mpirun -np $n_hosts -machinefile $machinefile /usr/bin/mpipython ~/script/annot/bin/MpiRpartValidation.py -k $schema -i $infname -j $output_file  -p $cp_ls -l $loss_ls -o $prior_ls -x $no_of_validations -s 0.9
+	echo ssh $HOSTNAME mpirun -np $n_hosts -machinefile $machinefile /usr/bin/mpipython ~/script/annot/bin/MpiRpartValidation.py -k $schema -i $infname -j $output_file  $parameter
 	#parallel on app2, nodes assigned by qsub
-	ssh $HOSTNAME mpirun -np $n_hosts -machinefile $machinefile /usr/bin/mpipython ~/script/annot/bin/MpiRpartValidation.py -k $schema -i $infname -j $output_file  -p $cp_ls -l $loss_ls -o $prior_ls -x $no_of_validations -s 0.9;;
+	ssh $HOSTNAME mpirun -np $n_hosts -machinefile $machinefile /usr/bin/mpipython ~/script/annot/bin/MpiRpartValidation.py -k $schema -i $infname -j $output_file $parameter;;
 	3)	n_hosts=6
 		machinefile=~/hostfile
-		echo ssh $HOSTNAME mpirun -np $n_hosts -machinefile $machinefile /usr/bin/mpipython ~/script/annot/bin/MpiRpartValidation.py -k $schema -i $infname -j $output_file  -p $cp_ls -l $loss_ls -o $prior_ls -x $no_of_validations -s 0.9
+		echo ssh $HOSTNAME mpirun -np $n_hosts -machinefile $machinefile /usr/bin/mpipython ~/script/annot/bin/MpiRpartValidation.py -k $schema -i $infname -j $output_file  $parameter
 		#parallel, 6 nodes from ~/hostfile
-		ssh $HOSTNAME mpirun -np $n_hosts -machinefile $machinefile /usr/bin/mpipython ~/script/annot/bin/MpiRpartValidation.py -k $schema -i $infname -j $output_file  -p $cp_ls -l $loss_ls -o $prior_ls -x $no_of_validations -s 0.9;;
+		ssh $HOSTNAME mpirun -np $n_hosts -machinefile $machinefile /usr/bin/mpipython ~/script/annot/bin/MpiRpartValidation.py -k $schema -i $infname -j $output_file  $parameter;;
 	*)	echo "MpiRpartValidation.py skipped";;	
 esac
 
