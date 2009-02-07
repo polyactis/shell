3f6f90b6a6a940ba6ee7b989e7f885ffca1ef67b
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2009-02-07 20:50:17 +0000

    add essid: 	USC Wireless 	kubi 	GlobalSuiteWireless 	FFGG
    git-svn-id: https://dl324b-1/svn/shell@198 12a06e64-900f-0410-8321-bc676669c086

--- trunk/wlan.sh
+++ trunk/wlan.sh
@@ -18,11 +18,18 @@ echo "5. hto_eth0"
 echo "6. w1141"
 echo "7. default"
 echo "8. ipam"
+echo "9. DLINK_WIRELESS"
+#echo "0. SMBE kubi"
+echo "0. aliso creek"
+echo "10. Big Sky, Montana"
+echo "11. FFGG"
 read a
 if [ $a = "1" ]; then
 	route del default gw src
-	iwconfig $interface essid "USC"
-	iwconfig $interface key s:GOUSC
+	iwconfig $interface essid "USC Wireless"
+	iwconfig $interface key open
+	iwconfig $interface key off
+	#iwconfig $interface key s:GOUSC
 	dhclient $interface
 fi
 if [ $a = "2" ]; then
@@ -76,4 +83,42 @@ if [ $a = "8" ]; then
 	iwconfig $interface essid "ipam"
 	iwconfig $interface key 6F737472696368657332313433
 	dhclient $interface
+fi
+
+if [ $a = "9" ]; then
+	ifconfig eth0 down
+	iwconfig $interface essid "DLINK_WIRELESS"
+	iwconfig $interface key off
+	dhclient $interface
+fi
+
+if [ $a = "199" ]; then
+	ifconfig eth0 down
+	iwconfig $interface essid "kubi"
+	iwconfig $interface key off
+	dhclient $interface
+fi
+
+if [ $a = "0" ]; then	#SMBE
+	ifconfig eth0 down
+	route del default gw src
+	#iwconfig $interface essid "u5czp568"
+	#iwconfig $interface essid CAULAINCOURT #2008 SMBE Barcelona
+	iwconfig $interface essid "ACI Wireless"
+	iwconfig $interface key off
+	dhclient $interface
+fi
+if [ $a = "10" ]; then
+	ifconfig eth0 down
+	iwconfig $interface essid "GlobalSuiteWireless"
+	iwconfig $interface key off
+	dhclient $interface
+fi
+if [ $a = "11" ]; then
+	ifconfig eth0 down
+	iwconfig $interface essid "FFGG"
+	iwconfig $interface key "2138393939"
+	#dhclient $interface
+	ifconfig ath0 192.168.10.177
+	route add default gw 192.168.10.2
 fi
