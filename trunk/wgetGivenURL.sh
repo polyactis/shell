943c09307ac808d817ac3899c683c16954ec6303
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2011-02-17 00:36:29 +0000

    Given URLs embedded in the file (lines beginned with # ignored), wget the contents recursively into TargetDirectory. 	Assume the URLs don't contain space. Not sure whether it'll be split if there is a space.
    git-svn-id: https://dl324b-1/svn/shell@215 12a06e64-900f-0410-8321-bc676669c086

---
+++ trunk/wgetGivenURL.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#$ -pe mpich 5
+
+if test $# -lt 2
+then
+	echo "Usage: wgetGivenURL.sh URLFile.txt TargetDirectory"
+	echo
+	echo "	Given URLs embedded in the file (lines beginned with # ignored), wget the contents recursively into TargetDirectory."
+	echo "	Assume the URLs don't contain space. Not sure whether it'll be split if there is a space."
+	exit
+fi
+
+urlFile=$1
+targetDirectory=$2
+
+parameter=''
+#while test -n "$6"
+#do
+#parameter=$parameter' '$6
+#shift
+#done
+
+#
+for i in `awk '{if (substr($0, 1,1)!="#") {print} }' $urlFile `; do
+	echo $i
+	wget --retr-symlinks -c -nd -P $targetDirectory $i/*
+done
