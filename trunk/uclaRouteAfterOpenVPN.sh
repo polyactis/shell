454ca27c80bb5e79ec8ca9078a1a0df44e6df19e
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2011-02-24 20:15:32 +0000

    add argument handling. could pass gateway addresses through commandline arguments
    git-svn-id: https://dl324b-1/svn/shell@223 12a06e64-900f-0410-8321-bc676669c086

--- trunk/uclaRouteAfterOpenVPN.sh
+++ trunk/uclaRouteAfterOpenVPN.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-###
-## 2011-2-23
-## this is for gateway setup after openvpn is established between ucla Office desktop and dl324b-1.cmb
-###
+if test $# -lt 2
+then
+	echo "Usage: $0 uclaGW openVPNDefaultGW"
+	echo
+	echo "	2011-2-23"
+	echo "	this is for gateway setup after openvpn is established between ucla Office desktop and dl324b-1.cmb"
+
+	echo
+	echo "Examples:	"
+	echo "		~/script//shell/uclarouteAfterOpenVPN.sh 149.142.212.1 10.8.0.5"
+exit
+fi
 #uclaGW=10.47.163.1
-uclaGW=149.142.212.1	#2011-2-23
-openVPNDefaultGW=10.8.0.9
+uclaGW=$1	#2011-2-23
+openVPNDefaultGW=$2
 route add -net 10.47.0.0 gw $uclaGW netmask 255.255.0.0
 route add -net 10.3.0.0 gw $uclaGW netmask 255.255.0.0
 route del default gw $openVPNDefaultGW
