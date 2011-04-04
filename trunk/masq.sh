2b4da9ed0d9d43ec771015eecb6e0616694771ef
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2011-04-04 23:40:48 +0000

    add arguments handling. now allow any given 255.255.255.0 subnetwork to be masqueraded
    git-svn-id: https://dl324b-1/svn/shell@230 12a06e64-900f-0410-8321-bc676669c086

--- trunk/masq.sh
+++ trunk/masq.sh
@@ -1,6 +1,21 @@
+#!/bin/sh
+if test $# -lt 1
+then
+	echo "Usage: $0 SUBNETWORK"
+	echo
+	echo "	2011-4-4"
+	echo "	script to enable masquerade (iptables -t nat) for a subnetwork."
+
+	echo
+	echo "Examples:	"
+	echo "		~/script/shell/masq.sh 192.168.3.0"
+exit
+fi
+subnetwork=$1
+
 echo 1 >/proc/sys/net/ipv4/ip_forward
 echo 1 >/proc/sys/net/ipv4/ip_dynaddr
 iptables -t nat -P POSTROUTING ACCEPT 
-iptables -t nat -A POSTROUTING  -s 10.100.113.147/32 -d ! 10.0.0.0/8 -j MASQUERADE
-iptables -t nat -A POSTROUTING -o eth1 -s 10.100.113.60/32 -d ! 10.0.0.0/8 -j MASQUERADE  
-iptables -t nat -A POSTROUTING -o eth1 -s 10.100.113.96/32 -d ! 10.0.0.0/8 -j MASQUERADE
+iptables -t nat -A POSTROUTING  -s $subnetwork/24 ! -d $subnetwork/24 -j MASQUERADE
+#iptables -t nat -A POSTROUTING -o eth1 -s 10.100.113.60/32 -d ! 10.0.0.0/8 -j MASQUERADE  
+#iptables -t nat -A POSTROUTING -o eth1 -s 10.100.113.96/32 -d ! 10.0.0.0/8 -j MASQUERADE
