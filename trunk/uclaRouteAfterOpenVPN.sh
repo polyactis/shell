efc1a508dc51ca75078a2d569fcc6d4bd08f572d
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2011-04-05 23:49:20 +0000

    the old default gateway is now guessed (gateway to 128.125.86.114). the vpn gateway is the one to 0.0.0.0
    no more arguments.
    
    
    git-svn-id: https://dl324b-1/svn/shell@235 12a06e64-900f-0410-8321-bc676669c086

--- trunk/uclaRouteAfterOpenVPN.sh
+++ trunk/uclaRouteAfterOpenVPN.sh
@@ -1,20 +1,42 @@
 #!/bin/sh
-if test $# -lt 2
+
+if test -z $1
 then
-	echo "Usage: $0 uclaGW openVPNDefaultGW"
+	#if $1 is empty, make up something. empty $1 would cause error reporting in 'test $argument="-h"'.
+	argument="whatever";
+else
+	argument=$1
+fi
+
+
+if test $argument = "-h"
+then
+	echo "Usage: $0"
 	echo
 	echo "	2011-2-23"
-	echo "	this is for gateway setup after openvpn is established between ucla Office desktop and dl324b-1.cmb"
+	echo "	this script removes the default gateway given by openvpn after vpn is established between ucla computers and dl324b-1.cmb"
 
 	echo
 	echo "Examples:	"
-	echo "		~/script//shell/uclarouteAfterOpenVPN.sh 149.142.212.1 10.8.0.5"
-exit
+	echo "		~/script//shell/uclarouteAfterOpenVPN.sh"
+	exit
 fi
-#uclaGW=10.47.163.1
-uclaGW=$1	#2011-2-23
-openVPNDefaultGW=$2
-route add -net 10.47.0.0 gw $uclaGW netmask 255.255.0.0
-route add -net 10.3.0.0 gw $uclaGW netmask 255.255.0.0
-route del default gw $openVPNDefaultGW
-route add default gw $uclaGW
+echo $#
+echo $@
+
+# 128.125.86.114 is where the vpn server is, its gateway is the original default gateway..
+uclaGW=`route -n |grep ^128.125.86.114|awk '{print $2}'`
+#assume now openvpn has replaced the default gateway with whatever it is. Even if it hasn't replaced it, it meant now harm.
+openVPNDefaultGW=`route -n |grep ^0.0.0.0|awk '{print $2}'`
+
+if test -n $openVPNDefaultGW
+then
+	echo route del default gw $openVPNDefaultGW
+	route del default gw $openVPNDefaultGW
+fi
+
+if test -n $uclaGW
+then
+	echo route add default gw $uclaGW
+	route add default gw $uclaGW
+fi
