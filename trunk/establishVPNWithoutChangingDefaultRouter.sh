e42b01f66bad40526cabd2de473e77cdb329b817
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2011-04-05 00:23:31 +0000

    script to establish an openvpn connection without changing the default gw
    git-svn-id: https://dl324b-1/svn/shell@232 12a06e64-900f-0410-8321-bc676669c086

---
+++ trunk/establishVPNWithoutChangingDefaultRouter.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# 2011-4-4
+# 	script to establish an openvpn connection without changing the default gw
+#
+#
+
+# must be executed before the openvpn init script
+oldDefaultGW=`route -n |grep ^0.0.0.0|awk '{print $2}'`
+echo /etc/init.d/openvpn start
+sleep 3;	#wait 3 seconds for the tun0 to be setup
+vpnDefaultGW=`route -n |grep ^0.0.0.0|awk '{print $2}'`
+echo route del default gw $vpnDefaultGW
+echo route add default gw $oldDefaultGW
