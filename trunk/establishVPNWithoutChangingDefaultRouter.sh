72f9c0ea4ca7e80d87b4a21fff37a82bb84aa559
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2011-04-06 00:53:15 +0000

    +# first look for the gateway to dl324b-1.cmb.usc.edu (vpn server), if not successful, look for default. +# this part is required if the openvpn is already running. The default gateway would be the vpn gateway. +oldDefaultGW=`route -n |grep ^128.125.86.114|awk '{print $2}'` + +if test -z $oldDefaultGW +then +	# must be executed before the openvpn init script +	oldDefaultGW=`route -n |grep ^0.0.0.0|awk '{print $2}'` +fi +
    git-svn-id: https://dl324b-1/svn/shell@236 12a06e64-900f-0410-8321-bc676669c086

--- trunk/establishVPNWithoutChangingDefaultRouter.sh
+++ trunk/establishVPNWithoutChangingDefaultRouter.sh
@@ -4,9 +4,17 @@
 #
 #
 
-# must be executed before the openvpn init script
-oldDefaultGW=`route -n |grep ^0.0.0.0|awk '{print $2}'`
-/etc/init.d/openvpn start
+# first look for the gateway to dl324b-1.cmb.usc.edu (vpn server), if not successful, look for default.
+# this part is required if the openvpn is already running. The default gateway would be the vpn gateway.
+oldDefaultGW=`route -n |grep ^128.125.86.114|awk '{print $2}'`
+
+if test -z $oldDefaultGW
+then
+	# must be executed before the openvpn init script
+	oldDefaultGW=`route -n |grep ^0.0.0.0|awk '{print $2}'`
+fi
+
+/etc/init.d/openvpn restart
 sleep 3;	#wait 3 seconds for the tun0 to be setup
 vpnDefaultGW=`route -n |grep ^0.0.0.0|awk '{print $2}'`
 echo route del default gw $vpnDefaultGW
