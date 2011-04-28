5b035211c899fc90cc7c9c8894af0c2013ee41dc
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2011-04-28 06:50:07 +0000

    user full hostname for hpc-cmb
    git-svn-id: https://dl324b-1/svn/shell@244 12a06e64-900f-0410-8321-bc676669c086

--- trunk/mount_hpc.sh
+++ trunk/mount_hpc.sh
@@ -2,5 +2,5 @@
 #sshfs -o reconnect yuhuang@hpc-cmb:/home/cmbpanfs-01/yuhuang ~/mnt/panfs/
 #sshfs -o reconnect yuhuang@hpc-cmb:./ ~/mnt/hpc-cmb/
 
-sshfs -o workaround=rename yuhuang@hpc-cmb:/home/cmbpanfs-01/yuhuang ~/mnt/panfs/
-sshfs -o workaround=rename yuhuang@hpc-cmb:./ ~/mnt/hpc-cmb/
+sshfs -o workaround=rename yuhuang@hpc-cmb.usc.edu:/home/cmbpanfs-01/yuhuang ~/mnt/panfs/
+sshfs -o workaround=rename yuhuang@hpc-cmb.usc.edu:./ ~/mnt/hpc-cmb/
