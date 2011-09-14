2c51ff1b9d0fdfcaca9077b90f648fd8106751cd
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2011-09-14 11:47:53 +0000

    replace hpc-cmb.usc.edu with hpc-login2.usc.edu
    git-svn-id: https://dl324b-1/svn/shell@260 12a06e64-900f-0410-8321-bc676669c086

--- trunk/mount_hpc.sh
+++ trunk/mount_hpc.sh
@@ -2,6 +2,6 @@
 #sshfs -o reconnect yuhuang@hpc-cmb:/home/cmbpanfs-01/yuhuang ~/mnt/panfs/
 #sshfs -o reconnect yuhuang@hpc-cmb:./ ~/mnt/hpc-cmb/
 
-sshfs -o workaround=rename yuhuang@hpc-cmb.usc.edu:/home/cmbpanfs-01/yuhuang ~/mnt/panfs/
-sshfs -o workaround=rename yuhuang@hpc-cmb.usc.edu:/ ~/mnt/hpc-cmb/
-sshfs -o workaround=rename yuhuang@hpc-cmb.usc.edu:/auto/cmb-03/mn/yuhuang ~/mnt/hpc-cmb_home/
+sshfs -o workaround=rename yuhuang@hpc-login2.usc.edu:/home/cmbpanfs-01/yuhuang ~/mnt/panfs/
+sshfs -o workaround=rename yuhuang@hpc-login2.usc.edu:/ ~/mnt/hpc-cmb/
+sshfs -o workaround=rename yuhuang@hpc-login2.usc.edu:/auto/cmb-03/mn/yuhuang ~/mnt/hpc-cmb_home/
