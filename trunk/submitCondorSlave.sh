8acf04fae05bf3e0d50a28a175a9d2005c902843
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2011-11-25 09:01:17 +0000

    point the condor job stdout&stderr output to qjob_output. sleeping interval during slave job submission forloop is now an optional argument
    git-svn-id: https://dl324b-1/svn/shell@272 12a06e64-900f-0410-8321-bc676669c086

--- trunk/submitCondorSlave.sh
+++ trunk/submitCondorSlave.sh
@@ -2,14 +2,15 @@
 
 noOfCpusPerNodeDefault=8
 noOfCondorSlavesDefault=100
-
+noOfSleepSecondsDefault=1800
 if test $# -lt 1 ; then
-	echo "  $0 masterHost [noOfCpusPerNode] [noOfCondorSlaves]"
+	echo "  $0 masterHost [noOfCpusPerNode] [noOfCondorSlaves] [noOfSleepSeconds]"
 	echo ""
 	echo "Note:"
 	echo "	#. masterHost is the node where the master is."
 	echo "	#. noOfCpusPerNode is passed to SGE on how many cpus to occupy on each node. But condor takes all cpus on each node. Default is $noOfCpusPerNodeDefault."
 	echo "	#. noOfCondorSlaves is the max number of condor slaves running/in SGE queue. Default is $noOfCondorSlavesDefault. The script goes into sleep and checks periodically to see whether it needs to submit another slave job."
+	echo "	#. noOfSleepSeconds is the number of seconds for this script to sleep before it submits another condor slave job. Default is $noOfSleepSecondsDefault."
 	exit 1
 fi
 masterHost=$1
@@ -23,32 +24,43 @@ if [ -z $noOfCondorSlaves ]
 then
 	noOfCondorSlaves=$noOfCondorSlavesDefault
 fi
+noOfSleepSeconds=$4
+if [ -z $noOfSleepSeconds ]
+then
+	noOfSleepSeconds=$noOfSleepSecondsDefault
+fi
 
 shellRepositoryPath=`dirname $0`
 noOfCondorJobs=`qstat -u polyacti|grep condor|wc -l|awk -F ' ' '{print $1}'`
-echo $noOfCondorJobs condor jobs now.
+echo $noOfCondorJobs condor jobs now, to reach $noOfCondorSlaves.
 
-while test $noOfCondorJobs -le $noOfCondorSlaves
+while test 1 -le 2
 do
-	currentUnixTime=`echo "import time; print time.time()"|python`
-	jobscriptFileName=/tmp/condorS.$currentUnixTime.sh
-	echo job script: $jobscriptFileName
-	cat >$jobscriptFileName <<EOF
+	if test $noOfCondorJobs -le $noOfCondorSlaves
+	then
+		echo "$noOfCpusPerNode cpus for each condor slave"
+		echo "Master is $masterHost"
+		currentUnixTime=`echo "import time; print time.time()"|python`
+		jobscriptFileName=/tmp/condorS.$currentUnixTime.sh
+		echo job script: $jobscriptFileName
+		cat >$jobscriptFileName <<EOF
 #!/bin/sh
 #$ -S /bin/bash
 #$ -cwd
-#$ -o \$JOB_NAME.joblog.\$JOB_ID
+#$ -o ~/qjob_output/\$JOB_NAME.joblog.\$JOB_ID
 #$ -j y
 #$ -l h_rt=23:59:00
 #$ -pe shared* $noOfCpusPerNode
 #$ -V
 source ~/.bash_profile
-$shellRepositoryPath/condor_launch/launch.sh 24 $masterHost
+$shellRepositoryPath/condor_launch/launch.sh 24 $noOfCpusPerNode $masterHost
 EOF
-	qsub $jobscriptFileName
-	#rm $jobscriptFileName
-	#$ -q eeskin_idre.q
-	sleep 10
+		qsub $jobscriptFileName
+		#rm $jobscriptFileName
+		#$ -q eeskin_idre.q
+	fi
+	echo "sleep now for $noOfSleepSeconds seconds"
+	sleep $noOfSleepSeconds
 	noOfCondorJobs=`qstat -u polyacti|grep condor|wc -l|awk -F ' ' '{print $1}'`
-	echo $noOfCondorJobs condor jobs now.
+	echo $noOfCondorJobs condor jobs now, to reach $noOfCondorSlaves.
 done
