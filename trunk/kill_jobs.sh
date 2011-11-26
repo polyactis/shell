906982e81ec61ec73071a6c0c7f139169748c470
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2011-11-26 23:21:41 +0000

    add a 3rd optional argument, forceKill, which instructs the script to kill without asking
    git-svn-id: https://dl324b-1/svn/shell@273 12a06e64-900f-0410-8321-bc676669c086

--- trunk/kill_jobs.sh
+++ trunk/kill_jobs.sh
@@ -2,25 +2,32 @@
 if test $# -lt 1
 then
 	echo "Usage:"
-	echo "	kill_jobs.sh PATTERN [KILLSIGNAL]"
+	echo "	kill_jobs.sh PATTERN [KILLSIGNAL] [forceKill]"
 	echo
 	echo "This script kills jobs by pattern-grepping (ps -ef|grep PATTERN)."
 	echo "	You will be asked to confirm before killing."
 	echo "	KILLSIGNAL is optional. Default is TERM/15."
+	echo "	forceKill is a toggle argument. Any-value will instruct the script to kill without asking."
 	exit
 fi
 
+pattern=$1
 if test -n "$2" ; then
 	kill_signal=$2
 else
 	kill_signal=15
 fi
-
-pattern=$1
+forceKill=$3
 echo "Processed to be killed are:"
 ps -ef |grep $pattern| awk '{print}'
-echo -n "Continue to kill?(Y/n):"
-read yes_or_no;
+
+if [ -z $forceKill ]
+then
+	echo -n "Continue to kill?(Y/n):"
+	read yes_or_no;
+else
+	yes_or_no="Y"
+fi
 if test -n "$yes_or_no"; then
 	yes_or_no=$yes_or_no
 else
