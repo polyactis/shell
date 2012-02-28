6e3f4c099e3af4831c5c617274e6e55621f0066b
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2012-02-28 18:26:18 +0000

    rename some variables and wrap everything inside a backup() function
    git-svn-id: https://dl324b-1/svn/shell@286 12a06e64-900f-0410-8321-bc676669c086

--- trunk/backup.sh
+++ trunk/backup.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 #argv[1] is the path for the directory.
 #argv[2] is the name of the tar file.
+backupFunc () {
 date
+inputFolder=$1
 echo -n 'Starting to backup '
-echo -n $1 
+echo -n $inputFolder
 echo -n ' ...'
-if test -n "$3"
+tarFileName=$2
+host_name=$3
+if test -z "$host_name"
 then
-	host_name=$3
-else
 	host_name=hpc-cmb.usc.edu
 fi
-echo "tar -cf /usr/local/src/zip/$2.tar $1"
-tar -cf /usr/local/src/zip/$2.tar $1
-echo "gzip /usr/local/src/zip/$2.tar"
-gzip /usr/local/src/zip/$2.tar
-echo "scp /usr/local/src/zip/$2.tar.gz yuhuang@$host_name:./backup/"
-scp /usr/local/src/zip/$2.tar.gz yuhuang@$host_name:./backup/
-echo "rm /usr/local/src/zip/$2.tar.gz"
-rm /usr/local/src/zip/$2.tar.gz
+echo "tar -cf /usr/local/src/zip/$tarFileName.tar $inputFolder"
+tar -cf /usr/local/src/zip/$tarFileName.tar $inputFolder
+echo "gzip /usr/local/src/zip/$tarFileName.tar"
+gzip /usr/local/src/zip/$tarFileName.tar
+echo "scp /usr/local/src/zip/$tarFileName.tar.gz yuhuang@$host_name:./backup/"
+scp /usr/local/src/zip/$tarFileName.tar.gz yuhuang@$host_name:./backup/
+echo "rm /usr/local/src/zip/$tarFileName.tar.gz"
+rm /usr/local/src/zip/$tarFileName.tar.gz
 echo 'done'
 date
+}
+backupFunc $*
