f92e980eb919a20af1055539c9cacddde7a18f78
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2012-05-12 02:57:00 +0000

    add "OT" to ps to list processes in time order
    git-svn-id: https://dl324b-1/svn/shell@292 12a06e64-900f-0410-8321-bc676669c086

--- trunk/kill_jobs.sh
+++ trunk/kill_jobs.sh
@@ -35,7 +35,7 @@ else
 fi
 echo $yes_or_no
 if test $yes_or_no = "Y" -o $yes_or_no = "y" -o $yes_or_no = "Yes" -o $yes_or_no = "yes" -o $yes_or_no = "YES" ; then
-	for i in `ps -ef |grep $pattern|awk '{print $2}'`; do
+	for i in `ps -ef OT |grep $pattern|awk '{print $2}'`; do
 		echo $i
 		kill -$kill_signal $i;
 	done
