7898bfcaca67b7ef5adcc58adaa54260be691d96
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2012-05-12 02:58:14 +0000

    kill cluster jobs that are running condor daemons (except the master node). It ssh into the node and kills all processes have keyword "condor" in it (ps -ef OT|grep condor).
    git-svn-id: https://dl324b-1/svn/shell@293 12a06e64-900f-0410-8321-bc676669c086

---
+++ trunk/killCondorJobs.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+clusterUsernameDefault=polyacti
+noOfCondorSlavesDefault=100
+noOfSleepSecondsDefault=180
+
+if test $# -lt 1 ; then
+	echo "  $0 masterHost [noOfCondorSlaves] [noOfSleepSeconds] [clusterUsername]"
+	echo ""
+	echo "Note:"
+	echo "	#. clusterUsername is the user who submits these condor jobs. Default is $clusterUsernameDefault."
+	echo "	#. masterHost is the node where the master is. If it equals =, it will try to get central manager hostname from ~/condor_central_manager.txt or become central manger itself if the latter is empty."
+	echo "	#. noOfCondorSlaves is the max number of condor slaves running/in SGE queue. Default is $noOfCondorSlavesDefault. The script goes into sleep and checks periodically to see whether it needs to submit another slave job."
+	echo "	#. noOfSleepSeconds is the number of seconds for this script to sleep before it submits another condor slave job. Default is $noOfSleepSecondsDefault."
+	exit 1
+fi
+masterHost=$1
+
+noOfCondorSlaves=$2
+if [ -z $noOfCondorSlaves ]
+then
+	noOfCondorSlaves=$noOfCondorSlavesDefault
+fi
+noOfSleepSeconds=$3
+if [ -z $noOfSleepSeconds ]
+then
+	noOfSleepSeconds=$noOfSleepSecondsDefault
+fi
+
+clusterUsername=$4
+if [ -z $clusterUsername ]
+then
+	clusterUsername=$clusterUsernameDefault
+fi
+echo clusterUsername is $clusterUsername.
+
+countCondorSJobs () {
+	echo `qstat -u polyacti|grep condor|wc -l|awk -F ' ' '{print $1}'`
+}
+findOneCondorJobNode() {
+	echo `qstat -u $clusterUsername|grep condor|head -n 1|awk -F ' ' '{print $8}'|awk -F '@' '{print $2}'`
+}
+shellRepositoryPath=`dirname $0`
+noOfCondorJobs=`countCondorSJobs`
+echo $noOfCondorJobs condor jobs now, reduce to $noOfCondorSlaves.
+
+while test $noOfCondorSlaves -le $noOfCondorJobs
+do
+	jobNode=`findOneCondorJobNode`
+	echo jobNode is $jobNode.
+	if test "$jobNode" != "$masterHost"
+	then
+		ssh $clusterUsername@$jobNode "~/script/shell/kill_jobs.sh condor"
+		#for i in `ps -ef OT|grep polyacti|grep condor|awk -F ' ' '{print $2}'`; do echo $i ; done
+	fi
+	echo "sleep now for $noOfSleepSeconds seconds"
+	sleep $noOfSleepSeconds
+	noOfCondorJobs=`countCondorSJobs`
+	echo $noOfCondorJobs condor jobs now, reduce to  $noOfCondorSlaves.
+done
