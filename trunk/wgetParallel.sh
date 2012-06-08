f3a5aa97482849ee01d0113d3632c9f320c8f220
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2012-06-08 03:59:23 +0000

    a parallel wget shell script
    git-svn-id: https://dl324b-1/svn/shell@299 12a06e64-900f-0410-8321-bc676669c086

---
+++ trunk/wgetParallel.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -o $JOB_NAME.joblog.$JOB_ID
+#$ -j y
+#$ -l time=200:00:00
+#$ -l highp
+#$ -r y
+#$ -V
+
+if test $# -lt 5
+then
+        echo "Usage: $0 USERNAME PASSWORD URL FILEWithToBeDownloadedFilenames TARGETDIR "
+        echo
+        echo
+	echo "This script calls wget to download every files within specific URL (no sub-folders) into target dir."
+	echo "Each file will be downloaded by one background wget process."
+	echo "The TARGETDIR will be made regardless of its existence."
+	echo
+        echo "Examples: "
+	echo "  $0 someone secret https://xfer.edu/gxfer2/ filesToBeDownloaded.txt genomeData/"
+exit
+fi
+
+
+username=$1
+password=$2
+URL=$3
+FILEWithToBeDownloadedFilenames=$4
+targetSubDir=$5
+mkdirhier $targetSubDir
+
+for i in `cat $FILEWithToBeDownloadedFilenames`;
+	do echo $i;
+	wget --user=cheerieyiemi --password=hiecaifoovio --recursive --no-parent --continue --reject='index.html*' $URL/$i --background -nd -P $targetSubDir -nH --cut-dirs=1;
+done
+
+# However, unlike -nd, --cut-dirs does not lose with subdirectories---for instance, with
+# -nH --cut-dirs=1, a beta/ subdirectory will be placed to xemacs/beta, as one would expect.
