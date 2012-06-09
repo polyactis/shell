c56699151ae7822dfe9a9b22138ea6733757795e
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2012-06-09 19:50:09 +0000

    classAd sshDBTunnel's value will be judged based on whether a tunnel exists or not.
    git-svn-id: https://dl324b-1/svn/shell@303 12a06e64-900f-0410-8321-bc676669c086

--- trunk/condor_launch/makeCondorConfig.sh
+++ trunk/condor_launch/makeCondorConfig.sh
@@ -114,8 +114,18 @@ echo "CONDOR_HOST=$CONDOR_HOST" >>$localCondorConfigFile
 echo "FILESYSTEM_DOMAIN=$CONDOR_HOST" >>$localCondorConfigFile
 echo "#FILESYSTEM_DOMAIN=\$(FULL_HOSTNAME)" >>$localCondorConfigFile
 echo "DAEMON_LIST=$CONDOR_DAEMON_LIST" >>$localCondorConfigFile
-#2012.4.16 add sshDBTunnel classAd for this machine
-echo "sshDBTunnel=$sshDBTunnel" >>$localCondorConfigFile
+
+noOfGrepLines=`ps -ef OT|grep dl324b-1.cmb.usc.edu:5432|wc -l`
+if test "$noOfGrepLines" = "2"; then	#sshTunnel is there. grep process will show up in ps -ef OT.
+	echo "sshDBTunnel=1" >>$localCondorConfigFile
+else
+	echo "sshDBTunnel=0" >>$localCondorConfigFile
+	#2012.4.16 add sshDBTunnel classAd for this machine
+	#echo "sshDBTunnel=$sshDBTunnel" >>$localCondorConfigFile
+	#ssh -N -L 5432:dl324b-1.cmb.usc.edu:5432 polyacti@login3 & 
+	#tunnelProcessID=0
+fi
+
 echo "STARTD_ATTRS = \$(STARTD_ATTRS) sshDBTunnel" >>$localCondorConfigFile
 # 2012.3.6 kill jobs immediately after preempt (when it's near the condor_master expiration time)
 #echo "PREEMPT = (CurrentTime - DaemonStartTime) > ($expirationInHours*\$(HOUR)-10*\$(MINUTE))" >>$localCondorConfigFile
