6dd423e3005c283cde36980a8e03c7c170a1bc64
yh <yh@12a06e64-900f-0410-8321-bc676669c086>
2012-07-06 22:46:52 +0000

    add argument ploneSubFolder to this script
    git-svn-id: https://dl324b-1/svn/shell@304 12a06e64-900f-0410-8321-bc676669c086

--- trunk/plone_backup.sh
+++ trunk/plone_backup.sh
@@ -1,31 +1,39 @@
 #!/bin/sh
 backupHostnameDefault=hpc-cmb.usc.edu
+ploneSubFolderDefault=zeocluster
 if test $# -lt 1 ; then
-	echo "  $0 ploneFolder [backupHostname]"
+	echo "  $0 ploneFolder [backupHostname] [ploneSubFolder]"
 	echo ""
 	echo "Note:"
 	echo "	ploneFolder is the path to the top plone folder."
 	echo "	backupHostname is the hostname of the machine where backup is to be sent through scp. Default is $backupHostnameDefault."
+	echo "	ploneSubFolder is the subfolder of ploneFolder, in which the plone instance or zeo cluster instance is installed. Default is $ploneSubFolderDefault."
 	echo "Examples:"
 	echo "	$0 /usr/local/Plone-4.0.4/"
+	echo "	$0 /usr/local/Plone-4.1.5/ hpc-cmb.usc.edu zinstance"
 	exit 1
 fi
 date
 ploneFolder=$1
 backupHostname=$2
+ploneSubFolder=$3
 echo -n 'Starting to backup '
-echo ' $ploneFolder  ...'
+echo ' $ploneFolder/$ploneSubFolder  ...'
 if test -z "$backupHostname"
 then
 	backupHostname=$backupHostnameDefault
 fi
+if test -z "$ploneSubFolder"
+then
+	ploneSubFolder=$ploneSubFolderDefault
+fi
 echo "backup host is $backupHostname."
-backupProgram=$ploneFolder/zeocluster/bin/backup
+backupProgram=$ploneFolder/$ploneSubFolder/bin/backup
 #2012.1.4 run the backup program
-$backupProgram
-backupLocalPath=$ploneFolder/zeocluster/var/backups/
-
 shellDir=`dirname $0`
+$backupProgram
+backupLocalPath=$ploneFolder/$ploneSubFolder/var/backups/
 $shellDir/backup.sh $backupLocalPath uclaOfficePloneBackup $backupHostname
 
-
+blobBackupLocalPath=$ploneFolder/$ploneSubFolder/var/blobstoragebackups
+$shellDir/backup.sh $blobBackupLocalPath uclaOfficePloneBlobBackup $backupHostname
