115cdc42bda250667653605f8f22f3990dc2f26b
Yu S. Huang <polyactis@gmail.com>
2013-01-14 10:15:25 -0800

    use login3 of hoffman2 , which is less utilized

--- trunk/mount_hoffman.sh
+++ trunk/mount_hoffman.sh
@@ -2,5 +2,5 @@
 #sshfs -o reconnect yuhuang@hpc-cmb:/home/cmbpanfs-01/yuhuang ~/mnt/panfs/
 #sshfs -o reconnect yuhuang@hpc-cmb:./ ~/mnt/hpc-cmb/
 
-sshfs -o workaround=rename polyacti@hoffman2.idre.ucla.edu:/ ~/mnt/hoffman2
-sshfs -o workaround=rename polyacti@hoffman2.idre.ucla.edu:./ ~/mnt/hoffman2_home
+sshfs -o workaround=rename polyacti@login3.hoffman2.idre.ucla.edu:/ ~/mnt/hoffman2
+sshfs -o workaround=rename polyacti@login3.hoffman2.idre.ucla.edu:./ ~/mnt/hoffman2_home
