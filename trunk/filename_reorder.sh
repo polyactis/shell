33a5280f1c54fb170a400caf4901e0a00a802ced
Yu S. Huang <polyactis@gmail.com>
2013-01-28 19:13:19 -0800

    Modify it so that it accepts arguments on the commandline. and it works on any folder, with user given new file suffix

--- trunk/filename_reorder.sh
+++ trunk/filename_reorder.sh
@@ -1,50 +1,84 @@
 #!/bin/bash
 
-echo "Warning: Before starting to rename the files,"
-echo "Warning: you should keep in mind the new filenames set"
-echo "Warning: should not overlap with the old filenames set."
-echo "Warning: Otherwise, file loss may happen."
-echo "Warning: The number of files within the folder <= 9999."
-echo "Warning: The new file suffix is .jpg. Will offer option in future."
-echo -n "Please enter the current prefix for all these files: "
-read oldPrefix
-echo -n "Please enter the new prefix for all these files: "
-read prefix
-echo -n "Enter the base number: "
-read base
-echo -n "What is the increasing unit, 1 or 2?"
-read unit
-page=$base
-count=$page
+pageNumberIncreasingStepDefault=2
+newFileSuffixDefault=.jpg
+if test $# -lt 4 ; then
+	echo "  $0 inputFolder currentFilePrefix newFilePrefix pageStartingNumber [pageNumberIncreasingStep] [newFileSuffix]"
+	echo ""
+	echo "Warning:"
+	echo "	1. Before starting to rename the files, you should keep in mind the new filenames set should not overlap with the old filenames set. Otherwise, file loss may happen."
+	echo "	2. The number of files selected within the folder should be <= 9999."
+	echo "Notes:"
+	echo "	1. pageNumberIncreasingStep is $pageNumberIncreasingStepDefault by default."
+	echo "	2. newFileSuffix is $newFileSuffixDefault by default."
+	echo "	3. new filenames look like newFilePrefix_pageNumber newFileSuffix"
+	echo
+	echo "Examples:"
+	echo "	Change all files in ~ with uni prefix to prefix=un and starting page number is 0. New filename is like un_0000.jpg"
+	echo "	$0 ~ uni un 0"
+	exit 1
+fi
 
-cd  $1
+inputFolder=$1
+currentFilePrefix=$2
+newFilePrefix=$3
+pageStartingNumber=$4
+pageNumberIncreasingStep=$5
+if [ -z $pageNumberIncreasingStep ]
+then
+	pageNumberIncreasingStep=$pageNumberIncreasingStepDefault
+fi
 
-echo -n "Current working directory is: "
-pwd
+newFileSuffix=$6
+if [ -z $newFileSuffix ]
+then
+	newFileSuffix=$newFileSuffixDefault
+fi
+
+#echo -n "Please enter the current prefix for all these files: "
+#read oldPrefix
+#echo -n "Please enter the new prefix for all these files: "
+#read prefix
+#echo -n "Enter the base number: "
+#read base
+#echo -n "What is the increasing unit, 1 or 2?"
+#read unit
+pageNumber=$pageStartingNumber
+count=$pageNumber
+
+#cd  $1
+
+echo "The folder is: $inputFolder"
+echo "Files to be changed has this prefix: $currentFilePrefix"
+echo "new file prefix: $newFilePrefix"
+echo "new file suffix: $newFileSuffix"
+echo "pageStartingNumber: $pageStartingNumber"
+echo "pageNumberIncreasingStep: $pageNumberIncreasingStep"
+
 echo -n "Continue? (y/n): "
 read answer
 
 if [ $answer = 'y' ]
 then
 
-	for i in `ls $oldPrefix*`
+	for i in `ls $inputFolder/$currentFilePrefix*`
 	do
 		if test -w $i
 		then
 			if `test -f $i`
 			then
-				if [ $page -lt 10 ]
+				if [ $pageNumber -lt 10 ]
 				then
-					page=000$page
-				elif [ $page -lt 100 ]
+					pageNumber=000$pageNumber
+				elif [ $pageNumber -lt 100 ]
 				then
-					page=00$page
-				elif [ $page -lt 1000 ]
+					pageNumber=00$pageNumber
+				elif [ $pageNumber -lt 1000 ]
 				then
-					page=0$page
+					pageNumber=0$pageNumber
 				fi
-				mv -i $i $prefix\_$page\.jpg
-				page=`expr $page \+ $unit`
+				mv -i $i $newFilePrefix\_$pageNumber$newFileSuffix
+				pageNumber=`expr $pageNumber \+ $pageNumberIncreasingStep`
 				count=`expr $count \+ 1`
 			fi
 		fi
@@ -54,4 +88,4 @@ else
 	echo "you answered no"
 fi
 
-echo `expr $count \- $base` "filenames reordered"
+echo `expr $count \- $pageStartingNumber` "filenames reordered"
