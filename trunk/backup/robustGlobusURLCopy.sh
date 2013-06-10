2721fb2831566b24b1e87aaa54a0ad2b083658fd
Yu S. Huang <polyactis@gmail.com>
2013-06-10 11:07:43 -0700

    added option "ToggleRecursive" to enable recursive running if non-Permission-Denied error is encountered.

--- trunk/backup/robustGlobusURLCopy.sh
+++ trunk/backup/robustGlobusURLCopy.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 
+source ~/.bash_profile
+
+#for shell debug purpose (display every commandline before it is executed)
 set -vx
-#source ~/.bash_profile
 
 if test $# -lt 2; then
 	echo "Usage:"
-	echo "  $0 SOURCE DESTINATION"
+	echo "  $0 SOURCE DESTINATION ToggleRecursive"
 	echo ""
 	echo "Note:"
 	echo "	#. This program restart the globus-url-copy if it exits non-zero (fail for whatever reason)."
-	echo "	#. append the star wildcard (*) in the SOURCE."
-	echo "	#. append the slash sign to DESTINATION."
+	echo "	#. Append a star wildcard (*) to SOURCE."
+	echo "	#. Always append a slash to DESTINATION."
+	echo "	#. ToggleRecursive: recursive running means if any error other than Permission Denied is encountered, the script would call itself again."
+	echo "	#. 	Set it to 1 to enable this recursive mode."
 	echo
 	echo "Examples:"
-	echo "	#"
+	echo "	# non-recursive"
 	echo "	$0 gsiftp://grid4.hoffman2.idre.ucla.edu/u/home/eeskin/namtran/[0-9a-zA-Z]* file:///h0/namtran/"
+	echo ""
+	echo "	# Recursive"
+	echo "	$0 gsiftp://grid4.hoffman2.idre.ucla.edu/u/home/eeskin/namtran/[0-9a-zA-Z]* file:///h0/namtran/ 1"
 	echo
 	echo
 	exit 1
 fi
 sourceDir=$1
 destinationDir=$2
+toggleRecursive=$3
+if test -z $toggleRecursive; then
+	toggleRecursive=0;	#default is 0
+fi
+echo toggleRecursive = $toggleRecursive
 
 globusURLCopyPath=`which globus-url-copy`
 commandLine="$globusURLCopyPath -sync-level 2 -sync -p 40 -vb -r $sourceDir $destinationDir"
@@ -40,7 +52,7 @@ $commandLine 2> $stderrFname
 exitCode=$?
 grep "Permission denied" $stderrFname
 grepExitCode=$?
-if test $exitCode != "0"; then
+if test $exitCode != "0" && test $toggleRecursive = "1"; then
 	cat $stderrFname
 	if test $grepExitCode != "0"; then
 		#keep going if it's not Permission denied error
